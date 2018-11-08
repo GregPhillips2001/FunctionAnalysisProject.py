b2379b18ec7f85f01f192986690febafba28c051
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2018-11-08 10:11:24 -0500

    Updated from Brython Server: 11/8/2018 10:11:23 AM

--- FunctionAnalysisProject.py
+++ FunctionAnalysisProject.py
@@ -1,3 +1,11 @@
 #Greg Phillips and Andrew Enelow
 #FunctionAnalysisProject.py
 #11/16/18
+
+print("Enter a function: ")
+function = input("")
+
+print("Enter a closed interval: ")
+interval = input("")
+
+
