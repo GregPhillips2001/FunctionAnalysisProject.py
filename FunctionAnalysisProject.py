f510c9240ee91af550d1ab5213b95eaeb12daff9
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2018-11-13 10:16:51 -0500

    Updated from Brython Server: 11/13/2018 10:16:50 AM

--- FunctionAnalysisProject.py
+++ FunctionAnalysisProject.py
@@ -13,8 +13,12 @@ print("Enter the left point of the interval: ")
 i2 = int(input(""))
 
 for x in range(i1, i2+1):
+    list = []
     y = eval(f)
+    list.append(y)
     print("f(",x,") =", y)
+    
+print(list)
     
     
 
