e2535c75ed1e75fb6cb85f8afba269a633f0fcfd
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2018-11-13 10:30:19 -0500

    Updated from Brython Server: 11/13/2018 10:30:17 AM

--- FunctionAnalysisProject.py
+++ FunctionAnalysisProject.py
@@ -3,13 +3,14 @@
 #11/16/18
 
 from math import *
+from random import randint
 
 print("Enter a function (in terms of x): ")
 f = input("")
 
 print("Enter the left point of the interval: ")
 i1 = int(input(""))
-print("Enter the left point of the interval: ")
+print("Enter the right point of the interval: ")
 i2 = int(input(""))
 
 list = []
@@ -18,13 +19,17 @@ for x in range(i1, i2+1):
     list.append(y)
     print("f(",x,") =", y)
 print(list)
-    
+h = .001   
 list2 = []
 for x in range(i1, i2+1):
-    x += 0.001
+    x += h
     y = eval(f)
     list2.append(y)
     print("f(",x,") =", y)
 print(list2)
-    
+
+len = i2-i1
+i = randint(1,len)
+der = (list2[i]-list[i])/h
+print("the slope is", der)
 
