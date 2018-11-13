238e9017c8e2a9530bb8909d6c8aad27fb4c4834
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2018-11-13 10:21:52 -0500

    Updated from Brython Server: 11/13/2018 10:21:51 AM

--- FunctionAnalysisProject.py
+++ FunctionAnalysisProject.py
@@ -17,8 +17,14 @@ for x in range(i1, i2+1):
     y = eval(f)
     list.append(y)
     print("f(",x,") =", y)
-    
 print(list)
     
+list2 = []
+for x in range(i1, i2+1):
+    x += 0.001
+    y = eval(f)
+    list2.append(y)
+    print("f(",x,"+ h) =", y)
+print(list2)
     
 
